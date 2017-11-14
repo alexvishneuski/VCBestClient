d19c9e21d4ba3f387fb01591fb872ab75eed788d
alexvishneuski <alexvishneuski@gmail.com>
2017-11-15 02:29:51 +0300

    renamed module app->view, added modules interactor & datamodel

--- view/src/main/java/com/github/alexvishneuski/vklayouts/MainActivity.java
+++ view/src/main/java/com/github/alexvishneuski/vklayouts/MainActivity.java
@@ -2,6 +2,10 @@ package com.github.alexvishneuski.vklayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.alexvishneuski.datamodel.Domain;
+import com.github.alexvishneuski.interactor.Test;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        Test test = new Test();
+        System.out.println(test.getS());
+
+        Domain domain = new Domain();
+        domain.testPrint();
     }
 }
