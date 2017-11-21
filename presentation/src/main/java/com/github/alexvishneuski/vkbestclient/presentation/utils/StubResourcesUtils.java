46f0f13832181864a0632b13a2bf18315db63186
alexvishneuski <alexvishneuski@gmail.com>
2017-11-21 17:00:17 +0400

    created StubResourcesUtils for recourse stub preparing

---
+++ presentation/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/utils/StubResourcesUtils.java
@@ -0,0 +1,18 @@
+package com.github.alexvishneuski.vkbestclient.presentation.utils;
+
+import android.graphics.Bitmap;
+
+
+public class StubResourcesUtils {
+
+    private String[] senderNames;
+    private String[] recipientNames;
+    private String[] sendingDates;
+    private String[] bodies;
+    private Bitmap[] contactAvatars;
+    private Bitmap userAvatar;
+    private String userName;
+    private Boolean isRead;
+
+
+}
