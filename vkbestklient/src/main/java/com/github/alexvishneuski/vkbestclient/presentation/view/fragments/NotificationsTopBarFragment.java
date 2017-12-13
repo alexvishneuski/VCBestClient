5ab4c2f236656273e6c3bc654f81b3985ca1c7f5
alexvishneuski <alexvishneuski@gmail.com>
2017-12-13 22:08:00 +0300

    created top bar notifications view with vector

---
+++ vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/view/fragments/NotificationsTopBarFragment.java
@@ -0,0 +1,33 @@
+package com.github.alexvishneuski.vkbestclient.presentation.view.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.alexvishneuski.vkbestclient.R;
+
+public class NotificationsTopBarFragment extends Fragment {
+
+    /*id of container in activity*/
+    private int mTopBarLayoutId;
+
+    /*view of this fragment*/
+    private View mView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        /*find id*/
+        mTopBarLayoutId = R.layout.fragment_top_bar_dialogs;
+
+        /*create view*/
+        mView = inflater.inflate(mTopBarLayoutId, null);
+
+        return mView;
+    }
+}
