cd4e4e5e1ba32c5688a06594573e4405cd4ec991
alexvishneuski <alexvishneuski@gmail.com>
2017-12-14 14:40:26 +0300

    renamed ViewModel to UIModel

--- vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/uimodel/DialogUIModel.java
+++ vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/uimodel/DialogUIModel.java
@@ -2,7 +2,7 @@ package com.github.alexvishneuski.vkbestclient.presentation.uimodel;
 
 import java.util.List;
 
-public class DialogViewModel {
+public class DialogUIModel {
 
     List<MessageInDialogListViewModel> mMessageList;
 
--- vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/uimodel/MessageDirectionViewModel.java
+++ vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/uimodel/MessageDirectionViewModel.java
@@ -1,6 +1,6 @@
 package com.github.alexvishneuski.vkbestclient.presentation.uimodel;
 
-/*for describing message flow in DialogViewModel */
+/*for describing message flow in DialogUIModel */
 public enum MessageDirectionViewModel {
 
     OUTGOING, INCOMING
