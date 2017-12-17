5f365ad7e10e4452e73417041e5fffa5513f7556
alexvishneuski <alexvishneuski@gmail.com>
2017-12-17 14:32:28 +0300

    added todo for getHistory's method's result parsing

---
+++ vkbestklient/src/main/java/com/github/alexvishneuski/vkbestclient/repository/networking/vkapi/model/objects/basicobjects/VKApiDialogHistory.java
@@ -0,0 +1,9 @@
+package com.github.alexvishneuski.vkbestclient.repository.networking.vkapi.model.objects.basicobjects;
+
+
+/**
+ * add model describing for
+ * list messages in one dialog model
+ * https://vk.com/dev/messages.getHistory?params[count]=20&params[user_id]=1743350&params[v]=5.69
+ */
+public class VKApiDialogHistory { }
