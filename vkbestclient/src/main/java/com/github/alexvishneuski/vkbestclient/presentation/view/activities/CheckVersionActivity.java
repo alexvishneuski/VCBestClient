4e4e830bd9fc62c4d366328f9c84dd557293f948
alexvishneuski <alexvishneuski@gmail.com>
2018-01-15 01:43:34 +0300

    added flavors

--- vkbestclient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/view/activities/CheckVersionActivity.java
+++ vkbestclient/src/main/java/com/github/alexvishneuski/vkbestclient/presentation/view/activities/CheckVersionActivity.java
@@ -2,16 +2,22 @@ package com.github.alexvishneuski.vkbestclient.presentation.view.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.github.alexvishneuski.vkbestclient.R;
 
 public class CheckVersionActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_version);
+        initView();
 
         goToAuthActivity();
+    }
+
+    private void initView() {
     }
 
     private void goToAuthActivity() {
